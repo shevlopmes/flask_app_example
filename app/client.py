9315c2f31b8a16d38817babb2fd37b7c1e27cee9
Tatiana Berlenko <tatiana.berlenko@gmail.com>
2024-11-05 10:17:49 +0200

    add client

--- app/client.py
+++ app/client.py
@@ -6,7 +6,7 @@ def check_root_url(url):
         if response.status_code == 400:
             print("Запрос успешен, код 200.")
         else:
-            print(f"Ожидался код 200, но получен код {response.status_code}.")
+            raise requests.RequestException(f"Ожидался код 200, но получен код {response.status_code}.")
     except requests.RequestException as e:
         print(f"Произошла ошибка при выполнении запроса: {e}")
 
